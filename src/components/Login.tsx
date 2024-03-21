52e7b128f4e457fe440c51c4dedb538e569f1c2d
PrajwalVK7 <prajwalvk98@gmail.com>
2024-03-21 13:02:31 +0530

    Login: Implemented Form handlinf for Login:

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+
+interface Logindata{
+    username:String;
+    password:String;
+}
 function Login() {
+
+    const [formData,setFormData] = useState<Logindata>({
+        username:"",
+        password:""
+    })
+
+    const handleLogin = (e: React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        const {username,password} = formData;
+        if(!username||!password){
+            alert("Please fill the form completely")
+        }
+        else{
+            alert("API")
+        }
+
+    }
     return (
         <div className='login-root d-flex justify-content-center align-items-center' style={{ height: '100vh' }}>
             <div  style={{height:'20rem',width:'20rem'}}  >
-                    <Form className='p-3 rounded container '>
+                    <Form className='p-3 rounded container ' onSubmit={handleLogin}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" placeholder="Enter username" />
+                            <Form.Control onChange={(e)=>setFormData({...formData,username:e.target.value})} type="text" placeholder="Enter username" />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control onChange={(e)=>setFormData({...formData,password:e.target.value})} type="password" placeholder="Password" />
                         </Form.Group>
 
                         <Button variant="primary" type="submit">
