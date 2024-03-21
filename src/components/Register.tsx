b1f63fb53e97d3c505fcd990795f715da805b9f8
PrajwalVK7 <prajwalvk98@gmail.com>
2024-03-21 12:53:47 +0530

    Register: Implemented Form Handling feture

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,32 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+
+interface RegisterData {
+    name:String;
+    address:String;
+    gender:String;
+    username:String;
+    password:String
+
+}
 function Register() {
+
+    const [formData, setFormData] = useState<RegisterData>({
+        name: "",
+        address: "",
+        gender: "",
+        username: "",
+        password: ""
+    });
+
+    const handleRegister = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const {name,address,gender,username,password} = formData;
+
+        if(!name||!address||!gender||!username ||!password){
+            alert("Please fill the form completely")
+        }
+        else{
+            alert("API")
+        }
+    }
+    
+
     return (
         <div className='login-root d-flex justify-content-center align-items-center' style={{ height: '100vh' }}>
             <div style={{ height: '20rem', width: '20rem' }}  >
-                <Form className='p-3 rounded container '>
+            <Form className="p-3 rounded container" onSubmit={handleRegister}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Your Full Name" />
+                        <Form.Control onChange={(e)=>setFormData({...formData,name:e.target.value})} type="text" placeholder="Enter Your Full Name" />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Address</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Your Address" />
+                        <Form.Control onChange={(e)=>setFormData({...formData,address:e.target.value})}  type="text" placeholder="Enter Your Address" />
                     </Form.Group>
-                    <Form.Select className="mb-3" aria-label="Default select example">
+                    <Form.Select onChange={(e)=>setFormData({...formData,gender:e.target.value})} className="mb-3" aria-label="Default select example">
                         <option>Gender</option>
-                        <option value="1">Male</option>
-                        <option value="2">Female</option>
+                        <option  value="male">Male</option>
+                        <option value="female">Female</option>
                     </Form.Select>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" placeholder="Enter username" />
+                        <Form.Control onChange={(e)=>setFormData({...formData,username:e.target.value})}  type="text" placeholder="Enter username" />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control onChange={(e)=>setFormData({...formData,password:e.target.value})}  type="password" placeholder="Password" />
                     </Form.Group>
 
                     <Button variant="primary" type="submit">
