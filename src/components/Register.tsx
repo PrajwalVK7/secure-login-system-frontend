3edba2829e20ae7efd2a92eecc11add7636de043
PrajwalVK7 <prajwalvk98@gmail.com>
2024-03-21 12:12:43 +0530

    UI of Register Compoenent Completed

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,9 +1,42 @@
 import React from 'react'
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 function Register() {
-  return (
-    <div>Register</div>
-  )
+    return (
+        <div className='login-root d-flex justify-content-center align-items-center' style={{ height: '100vh' }}>
+            <div style={{ height: '20rem', width: '20rem' }}  >
+                <Form className='p-3 rounded container '>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Your Full Name" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Address</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Your Address" />
+                    </Form.Group>
+                    <Form.Select className="mb-3" aria-label="Default select example">
+                        <option>Gender</option>
+                        <option value="1">Male</option>
+                        <option value="2">Female</option>
+                    </Form.Select>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="text" placeholder="Enter username" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Register
+                    </Button>
+                </Form>
+            </div>
+
+
+        </div>)
 }
 
 export default Register
